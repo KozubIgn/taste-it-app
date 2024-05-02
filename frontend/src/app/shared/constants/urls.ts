25deaac7cfa47b514ef48e845f342cc80c5ce478
KozubIgn <ignacykozub@gmail.com>
2024-05-02 15:50:33 +0200

    add new api url

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -3,6 +3,7 @@ const BASE_URL = 'http://localhost:5000';
 export const RECIPES = BASE_URL + '/api/recipes';
 export const RECIPE_ADD_NEW = (userId: string) => `${RECIPES}/${userId}/recipe/new`;
 export const RECIPE_DELETE = (userId: string, recipeId: string) => `${RECIPES}/${userId}/recipes/${recipeId}`;
+export const RECIPE_UPDATE = (userId: string, recipeId: string) => `${RECIPES}/${userId}/recipes/${recipeId}`;
 export const RECIPES_BY_SEARCH_URL = RECIPES + '/search/'; // behind slash leave an empty space for adding searchTeerm
 export const RECIPES_BY_ID = BASE_URL + 'api/recipes/';
 export const TAGS_URL = BASE_URL + '/api/tags';
