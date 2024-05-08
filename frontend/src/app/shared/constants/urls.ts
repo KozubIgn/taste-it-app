ab16ef90506e81d573667336cef4984c63916093
KozubIgn <ignacykozub@gmail.com>
2024-05-08 17:28:14 +0200

    add FAVOURITES url in urls.ts

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -4,6 +4,7 @@ export const RECIPES = BASE_URL + '/api/recipes';
 export const RECIPE_ADD_NEW = (userId: string) => `${RECIPES}/${userId}/recipe/new`;
 export const RECIPE_DELETE = (userId: string, recipeId: string) => `${RECIPES}/${userId}/recipes/${recipeId}`;
 export const RECIPE_UPDATE = (userId: string, recipeId: string) => `${RECIPES}/${userId}/recipes/${recipeId}`;
+export const FAVOURITES = (recipeId: string) => `${RECIPES}/${recipeId}/favourites`;
 export const RECIPES_BY_SEARCH_URL = RECIPES + '/search/'; // behind slash leave an empty space for adding searchTeerm
 export const RECIPES_BY_ID = BASE_URL + 'api/recipes/';
 export const TAGS_URL = BASE_URL + '/api/tags';
