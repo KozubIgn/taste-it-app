4c8cbbf9660b2cdaa3695073a523147d76d3355a
KozubIgn <ignacykozub@gmail.com>
2024-05-09 16:25:49 +0200

    import MatCheckBoxModule

--- frontend/src/app/shared/modules/material.module.ts
+++ frontend/src/app/shared/modules/material.module.ts
@@ -3,19 +3,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatSlideToggleModule,
         MatChipsModule,
-        MatDialogModule
+        MatDialogModule,
+        MatCheckboxModule
     ],
     exports: [
         MatButtonModule,
         MatSlideToggleModule,
         MatChipsModule,
-        MatDialogModule
+        MatDialogModule,
+        MatCheckboxModule
     ]
 })
 
