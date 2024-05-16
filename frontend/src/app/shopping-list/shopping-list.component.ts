98e1af9ec379b4776994d4c7ce4f26be8f6d3a51
KozubIgn <ignacykozub@gmail.com>
2024-05-16 14:01:43 +0200

    change name on subscription property in shopping-list.component.ts

--- frontend/src/app/shopping-list/shopping-list.component.ts
+++ frontend/src/app/shopping-list/shopping-list.component.ts
@@ -114,6 +114,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.ingredientChangeSub?.unsubscribe();
+    this.shoppingListsChangeSub?.unsubscribe();
   }
 }
